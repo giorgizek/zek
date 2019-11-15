7ce764c1d96b4b31cca93ba7e0e0316d8314d06a
Giorgi Zekalashvili <giorgizek@gmail.com>
2019-11-15 22:10:30 +0200

    Added login loc classes

---
+++ src/Zek/Model/Identity/LoginLog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Zek.Data.Entity;
+
+namespace Zek.Model.Identity
+{
+    public class LoginLog
+    {
+        public int Id { get; set; }
+        public string Ip { get; set; }
+        public int Count { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class LoginLogMap : EntityTypeMap<LoginLog>
+    {
+        public LoginLogMap(ModelBuilder builder) : base(builder)
+        {
+            ToTable("LoginLogs", "Identity");
+            HasKey(x => x.Id);
+
+            Property(x => x.Ip).HasMaxLength(46).IsRequired();
+            Property(x => x.Date).HasColumnTypeDateTime();
+        }
+    }
+
+    public class UserLoginLog
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Ip { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class UserLoginLogMap : EntityTypeMap<UserLoginLog>
+    {
+        public UserLoginLogMap(ModelBuilder builder) : base(builder)
+        {
+            ToTable("UserLoginLogs", "Identity");
+            HasIndex(x => x.Id);
+
+            Property(u => u.NormalizedUserName).HasMaxLength(256);
+            Property(x => x.Ip).HasMaxLength(46).IsRequired();
+            Property(x => x.Date).HasColumnTypeDateTime();
+        }
+    }
+}
