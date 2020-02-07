f00f118602734a9e0a3cee3ff1fff8d42d018f3e
Giorgi Zekalashvili <giorgizek@gmail.com>
2020-02-07 18:14:27 +0200

    Changed model state to asp.net core modeldictionary

---
+++ src/Zek/Model/ModelStateResult.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace Zek.Model
+{
+    [Serializable]
+    public class ModelStateResult<TModel> : ModelStateResult
+    {
+        public ModelStateResult(TModel value)
+        {
+            Value = value;
+        }
+
+        public ModelStateResult()
+        {
+        }
+
+
+        public TModel Value { get; set; }
+    }
+
+    [Serializable]
+    public class ModelStateResult :  ModelStateDictionary
+    {
+        public StatusCode StatusCode { get; set; }
+
+        public void Add(string key, Enum @enum) => Add(key, @enum.ToString());
+        public void Add(string key, string errorMessage)
+        {
+            AddModelError(key, errorMessage);
+
+            if (StatusCode == StatusCode.OK)
+                StatusCode = StatusCode.BadRequest;
+        }
+    }
+}
--- src/Zek/Web/BaseController.cs
+++ src/Zek/Web/BaseController.cs
@@ -1,29 +1,68 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Zek.Extensions;
 using Zek.Extensions.Security.Claims;
+using Zek.Model;
 
 namespace Zek.Web
 {
     public class BaseController : Controller
     {
-        private int? _userId;
-        protected virtual int UserId
+        //private int? _userId;
+        //protected virtual int UserId
+        //{
+        //    get
+        //    {
+        //        if (_userId == null)
+        //            _userId = User.GetUserId().ToInt32();
+        //        return _userId.Value;
+        //    }
+        //    set => _userId = value;
+        //}
+
+        //private string[] _roles;
+        //public string[] Roles
+        //{
+        //    get => _roles ??= User.GetRoles().ToArray();
+        //    set => _roles = value;
+        //}
+
+
+        /// <summary>
+        /// Creates an <see cref="OkObjectResult"/> or <see cref="NotFoundResult"/> object that produces an <see cref="StatusCodes.Status200OK"/> or <see cref="StatusCodes.Status404NotFound"/> response.
+        /// </summary>
+        /// <param name="value">The content value to format in the entity body.</param>
+        /// <returns>The created <see cref="OkObjectResult"/> or <see cref="NotFoundResult"/> for the response.</returns>
+        [NonAction]
+        public virtual IActionResult Auto(object value)
         {
-            get
-            {
-                if (_userId == null)
-                    _userId = User.GetUserId().ToInt32();
-                return _userId.Value;
-            }
-            set => _userId = value;
+            return value != null
+                ? new OkObjectResult(value)
+                : (IActionResult)NotFound();
         }
 
-        private string[] _roles;
-        public string[] Roles
+        [NonAction]
+        public virtual IActionResult Auto<T>(ModelStateResult<T> modelStateResult)
         {
-            get => _roles ??= User.GetRoles().ToArray();
-            set => _roles = value;
+            switch (modelStateResult.StatusCode)
+            {
+                case Model.StatusCode.BadRequest:
+                    return new BadRequestObjectResult(modelStateResult);
+
+                case Model.StatusCode.Forbidden:
+                    return new ForbidResult();
+
+                case Model.StatusCode.NotFound:
+                    return new NotFoundObjectResult(modelStateResult);
+
+                case Model.StatusCode.InternalServerError:
+                    return new ObjectResult(modelStateResult) { StatusCode = modelStateResult.StatusCode.ToInt32() };
+
+                //case StatusCode.OK:
+                default:
+                    return new OkObjectResult(modelStateResult.Value);
+            }
         }
     }
 }
