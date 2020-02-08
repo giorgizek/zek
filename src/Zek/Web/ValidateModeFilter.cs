ab7feb02d53e17ea36e723e3de5450ccd2bc5bca
Giorgi Zekalashvili <giorgizek@gmail.com>
2020-02-08 17:26:01 +0200

    Validation filter

---
+++ src/Zek/Web/ValidateModeFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Zek.Web
+{
+    public class ValidateModeFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // execute any code before the action executes
+            if (!context.ModelState.IsValid)
+            {
+                context.Result =  new BadRequestObjectResult(context.ModelState);
+            }
+            else
+            {
+                await next();
+            }
+
+            //var result = await next();
+            // execute any code after the action executes
+        }
+    }
+}
