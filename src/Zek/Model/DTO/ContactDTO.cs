de2bca5588e0cfa17f8d0349c89be2b23f9e2fbc
Giorgi Zekalashvili <giorgizek@gmail.com>
2020-02-13 12:15:47 +0200

    Splitted Contact DTO

--- src/Zek/Model/DTO/ContactDTO.cs
+++ src/Zek/Model/DTO/ContactDTO.cs
@@ -1,32 +1,74 @@
 ﻿namespace Zek.Model.DTO
 {
-    public class ContactDTO
+    public class ContactBaseDTO
     {
         public int? Id { get; set; }
+        /// <summary>
+        /// Phone 1
+        /// </summary>
         public string Phone1 { get; set; }
 
+        /// <summary>
+        /// Fax 1
+        /// </summary>
         public string Fax1 { get; set; }
 
+        /// <summary>
+        /// Mobile 1
+        /// </summary>
         public string Mobile1 { get; set; }
 
+        /// <summary>
+        /// Email 1
+        /// </summary>
         public string Email1 { get; set; }
+    }
 
+    public class ContactDTO : ContactBaseDTO
+    {
+        /// <summary>
+        /// Homepage or personal page
+        /// </summary>
         public string Url { get; set; }
 
+        /// <summary>
+        /// Phone 2
+        /// </summary>
         public string Phone2 { get; set; }
 
+        /// <summary>
+        /// Phone 3
+        /// </summary>
         public string Phone3 { get; set; }
 
+        /// <summary>
+        /// Fax 2
+        /// </summary>
         public string Fax2 { get; set; }
 
+        /// <summary>
+        /// Fax 3
+        /// </summary>
         public string Fax3 { get; set; }
 
+        /// <summary>
+        /// Mobile 2
+        /// </summary>
         public string Mobile2 { get; set; }
 
+        /// <summary>
+        /// Mobile 3
+        /// </summary>
         public string Mobile3 { get; set; }
 
+        /// <summary>
+        /// Email 2
+        /// </summary>
         public string Email2 { get; set; }
 
+        /// <summary>
+        /// Email 3
+        /// </summary>
         public string Email3 { get; set; }
     }
 }
