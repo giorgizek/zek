7d6f84fbed8622fcbb382d51391451202452fcfa
Giorgi Zekalashvili <47349388+giorgizek@users.noreply.github.com>
2020-11-04 17:16:49 +0200

    Started Dynamic Grid

---
+++ src/Zek/Data/FormatType.cs
@@ -0,0 +1,14 @@
+﻿namespace Zek.Data
+{
+    public enum FormatType
+    {
+        None = 0,
+        Numeric = 1,
+        DateTime = 2,
+
+        Custom = 3
+    }
+
+
+   
+}
---
+++ src/Zek/Grid/Columns/GridColumn.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zek.Data.Entity;
+
+namespace Zek.Grid.Columns
+{
+    public class GridColumn
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FormatString { get; set; }
+        public int FormatType { get; set; }
+        public bool Visible { get; set; }
+        public int VisibleIndex { get; set; }
+        public bool Required { get; set; }
+        public bool AllowEdit { get; set; }
+    }
+
+    public class GridColumnBaseMap : EntityTypeMap<GridColumn>
+    {
+        public GridColumnBaseMap(ModelBuilder builder) : base(builder)
+        {
+            Property(x => x.Id).ValueGeneratedOnAdd();
+            Property(x => x.Name).HasMaxLength(200);
+            Property(x => x.FormatString).HasMaxLength(200);
+        }
+    }
+}
