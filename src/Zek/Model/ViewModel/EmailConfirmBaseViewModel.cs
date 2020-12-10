1c110927ccbb2e326d5bdbb9c1a23143d2d005cd
Giorgi Zekalashvili <47349388+giorgizek@users.noreply.github.com>
2020-12-10 23:42:29 +0400

    Create EmailConfirmBaseViewModel.cs

---
+++ src/Zek/Model/ViewModel/EmailConfirmBaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zek.Model.ViewModel
+{
+    public class EmailConfirmBaseViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string IdNumber { get; set; }
+        public string Company { get; set; }
+        public string Website { get; set; }
+        public string ConfirmUrl { get; set; }
+        public DateTime ValidTo { get; set; }
+    }
+}
