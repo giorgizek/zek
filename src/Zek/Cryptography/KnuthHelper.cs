63a93db475c80f7a5b536cb6f4e439811faa6bbf
Giorgi Zekalashvili <47349388+giorgizek@users.noreply.github.com>
2021-01-10 22:53:01 +0400

    Create KnuthHelper.cs

---
+++ src/Zek/Cryptography/KnuthHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Zek.Cryptography
+{
+    public static class KnuthHelper
+    {
+        public static string KnuthHex(string plainText) => KnuthHex(Encoding.UTF8.GetBytes(plainText));
+        public static string KnuthHex(byte[] bytes)
+        {
+            return CalculateHash(bytes).ToString("X2");
+        }
+
+
+        public static long CalculateHash(string plainText) => CalculateHash(Encoding.Default.GetBytes(plainText));
+        public static long CalculateHash(byte[] bytes)
+        {
+            unchecked
+            {
+                var hashedValue = 3074457345618258791UL;
+                foreach (var b in bytes)
+                {
+                    hashedValue += b;
+                    hashedValue *= 3074457345618258799UL;
+                }
+                return (long)hashedValue;
+            }
+        }
+    }
+}
