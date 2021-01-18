149b27aad66705a56a84f36cc3256fdf4c524e8f
Giorgi Zekalashvili <47349388+giorgizek@users.noreply.github.com>
2021-01-19 01:42:26 +0400

    Order statuses

--- src/Zek/Accounting/PaymentStatus.cs
+++ src/Zek/Accounting/PaymentStatus.cs
@@ -1,17 +1,33 @@
 ﻿namespace Zek.Accounting
 {
+    /// <summary>
+    /// Represents a payment status enumeration
+    /// </summary>
     public enum PaymentStatus
     {
+        /// <summary>
+        /// Pending
+        /// </summary>
         Pending = 10,
-
+        /// <summary>
+        /// Authorized
+        /// </summary>
         Authorized = 20,
-
+        /// <summary>
+        /// Paid
+        /// </summary>
         Paid = 30,
-
+        /// <summary>
+        /// Partially Refunded
+        /// </summary>
         PartiallyRefunded = 35,
-
+        /// <summary>
+        /// Refunded
+        /// </summary>
         Refunded = 40,
-        
-        Voided = 50
+        /// <summary>
+        /// Voided
+        /// </summary>
+        Voided = 50,
     }
 }
