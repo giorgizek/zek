7fdf527570c978d92e1a3bf29d8eed2bab00a3fc
Giorgi Zekalashvili <giorgizek@gmail.com>
2021-09-09 21:12:38 +0400

    Update DateTimeHelper.cs

--- src/Zek/Utils/DateTimeHelper.cs
+++ src/Zek/Utils/DateTimeHelper.cs
@@ -86,6 +86,89 @@ namespace Zek.Utils
         }
 
 
+
+
+        public static bool HasInside(TimeSpan start, TimeSpan end, TimeSpan date)
+        {
+            return date >= start && date <= end;
+        }
+     
+        public static bool HasInside(TimeSpan start1, TimeSpan end1, TimeSpan start2, TimeSpan end2)
+        {
+            return HasInside(start1, end1, start2) && HasInside(start1, end1, end2);
+        }
+
+        public static bool Intersects(TimeSpan start1, TimeSpan end1, TimeSpan start2, TimeSpan end2)
+        {
+            return
+                HasInside(start1, end1, start2) ||
+                HasInside(start1, end1, end2) ||
+                (start2 < start1 && end2 > end1);
+        }
+
+        public static bool Overlaps(TimeSpan start1, TimeSpan end1, TimeSpan start2, TimeSpan end2)
+        {
+            var relation = GetRelation(start1, end1, start2, end2);
+            return
+                relation != PeriodRelation.After &&
+                relation != PeriodRelation.StartTouching &&
+                relation != PeriodRelation.EndTouching &&
+                relation != PeriodRelation.Before;
+        }
+
+        public static PeriodRelation GetRelation(TimeSpan start1, TimeSpan end1, TimeSpan start2, TimeSpan end2)
+        {
+            if (end2 < start1)
+            {
+                return PeriodRelation.After;
+            }
+            if (start2 > end1)
+            {
+                return PeriodRelation.Before;
+            }
+            if (start2 == start1 && end2 == end1)
+            {
+                return PeriodRelation.ExactMatch;
+            }
+            if (end2 == start1)
+            {
+                return PeriodRelation.StartTouching;
+            }
+            if (start2 == end1)
+            {
+                return PeriodRelation.EndTouching;
+            }
+            if (HasInside(start1, end1, start2, end2))
+            {
+                if (start2 == start1)
+                {
+                    return PeriodRelation.EnclosingStartTouching;
+                }
+                return end2 == end1 ? PeriodRelation.EnclosingEndTouching : PeriodRelation.Enclosing;
+            }
+            var periodContainsMyStart = HasInside(start2, end2, start1);
+            var periodContainsMyEnd = HasInside(start2, end2, end1);
+            if (periodContainsMyStart && periodContainsMyEnd)
+            {
+                if (start2 == start1)
+                {
+                    return PeriodRelation.InsideStartTouching;
+                }
+                return end2 == end1 ? PeriodRelation.InsideEndTouching : PeriodRelation.Inside;
+            }
+            if (periodContainsMyStart)
+            {
+                return PeriodRelation.StartInside;
+            }
+            if (periodContainsMyEnd)
+            {
+                return PeriodRelation.EndInside;
+            }
+            throw new InvalidOperationException("invalid period relation of '" + start1 + "-" + end1 + "' and '" + start2 + "-" + end2 + "'");
+        }
+
+
+
         [Obsolete("Use Overlaps or Intersects instead")]
         public static bool Overlap(DateTime start1, DateTime end1, DateTime start2, DateTime end2)
         {
