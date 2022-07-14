c2c6cd431e6187294d7448d34e887e38bd5d6dae
Giorgi Zekalashvili <giorgizek@gmail.com>
2022-07-15 01:14:25 +0400

    updated nuget

--- src/Zek/Model/DTO/Person/PersonsDTO.cs
+++ src/Zek/Model/DTO/Person/PersonsDTO.cs
@@ -1,30 +1,18 @@
 ﻿using System;
-using Zek.Model.Dictionary;
 
 namespace Zek.Model.DTO.Person
 {
     public class PersonsDTO
     {
         public int Id { get; set; }
-
         public string FullName { get; set; }
-
         public string PersonalNumber { get; set; }
-
-
         public int? GenderId { get; set; }
-
         public DateTime? BirthDate { get; set; }
-
-
         public string Phone1 { get; set; }
-
         public string Fax1 { get; set; }
-
         public string Mobile1 { get; set; }
-
         public string Email1 { get; set; }
-
         public bool IsLegal { get; set; }
         public bool IsDeleted { get; set; }
     }
