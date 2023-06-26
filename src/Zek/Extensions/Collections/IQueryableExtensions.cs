31f22b96b2774611ef3c420c95c05e364ad3e53f
Giorgi Zekalashvili <giorgizek@gmail.com>
2023-06-26 13:54:12 +0400

    Create IQueryableExtensions.cs

---
+++ src/Zek/Extensions/Collections/IQueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zek.Extensions.Collections
+{
+    public static class IQueryableExtensions
+    {
+        /// <summary>
+        /// Execute the remote query and materialize the results.
+        /// </summary>
+        /// <typeparam name="T">The type of the query.</typeparam>
+        /// <param name="query">The base query to run.</param>
+        /// <returns>The result.</returns>
+        public static async Task<HashSet<T>> ToHashSetAsync<T>(IQueryable<T> query) => new HashSet<T>(await query.ToArrayAsync());
+    }
+}
