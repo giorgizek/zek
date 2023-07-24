08b5ea6fb7fb687438baa518fce1dcab737f4b07
Giorgi Zekalashvili <giorgizek@gmail.com>
2023-07-25 01:10:15 +0400

    Update ConfirmBaseViewModel.cs

--- src/Zek/Model/ViewModel/ConfirmBaseViewModel.cs
+++ src/Zek/Model/ViewModel/ConfirmBaseViewModel.cs
@@ -7,6 +7,7 @@ namespace Zek.Contracts
         public string Application { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; }
         public string UserName { get; set; }
         public string IdNumber { get; set; }
         public string PhoneNumber { get; set; }
