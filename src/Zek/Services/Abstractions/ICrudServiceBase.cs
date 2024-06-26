77c86174a86cf51397a07e12f1ae7b5f3ecf0214
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-06-26 19:54:06 +0400

    CrudService

--- src/Zek/Services/Abstractions/ICrudServiceBase.cs
+++ src/Zek/Services/Abstractions/ICrudServiceBase.cs
@@ -6,14 +6,19 @@ using Zek.PagedList;
 namespace Zek.Services.Abstractions
 {
     public interface ICrudServiceBase<TFilter, TListItem, TItem>
-        : ICrudServiceBase<TFilter, TListItem, TItem, IApiResponse>
+        : ICrudServiceBase<int, TFilter, TListItem, TItem>
+    {
+    }
+    public interface ICrudServiceBase<TId, TFilter, TListItem, TItem>
+        : ICrudServiceBase<TId, TFilter, TListItem, TItem, IApiResponse, IApiResponse>
     {
     }
 
-    public interface ICrudServiceBase<TFilter, TListItem, TItem, TSaveResponse>
+    public interface ICrudServiceBase<TId, TFilter, TListItem, TItem, TSaveResponse, TDeleteResponse>
     {
         Task<IPagedList<TListItem>> GetAsync(TFilter filter, CancellationToken cancellationToken = default);
-        Task<TItem?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<TItem?> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
         Task<TSaveResponse> SaveAsync(TItem model, CancellationToken cancellationToken = default);
+        Task<TDeleteResponse> DeleteAsync(TId id, CancellationToken cancellationToken = default);
     }
 }
