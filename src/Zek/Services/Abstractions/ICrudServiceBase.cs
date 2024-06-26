f80f39fce2192392a42d463b63aa017f57ade133
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-06-26 11:11:09 +0400

    ICrudServiceBase

---
+++ src/Zek/Services/Abstractions/ICrudServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Zek.Contracts;
+using Zek.PagedList;
+
+namespace Zek.Services.Abstractions
+{
+    public interface ICrudServiceBase<TFilter, TListItem, TItem>
+        : ICrudServiceBase<TFilter, TListItem, TItem, IApiResponse>
+    {
+    }
+
+    public interface ICrudServiceBase<TFilter, TListItem, TItem, TSaveResponse>
+    {
+        Task<IPagedList<TListItem>> GetAsync(TFilter filter, CancellationToken cancellationToken = default);
+        Task<TItem?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<TSaveResponse> SaveAsync(TItem model, CancellationToken cancellationToken = default);
+    }
+}
