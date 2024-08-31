586901196b8dc384ab27efd62d98b7db7b08df54
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-08-31 14:10:49 +0400

    Base64StringFileDto

--- src/Zek/Contracts/FileDto.cs
+++ src/Zek/Contracts/FileDto.cs
@@ -1,16 +1,22 @@
 ﻿namespace Zek.Contracts
 {
-    public class FileDto<TId>
+    public class FileDto<TId, TContent>
     {
         public TId Id { get; set; }
         public string FileName { get; set; }
         public int? FileSize { get; set; }
         public string Extension { get; set; }
         public string MediaType { get; set; }
-        public byte[] Content { get; set; }
+        public TContent Content { get; set; }
         public bool? IsDeleted { get; set; }
     }
+    public class FileDto<TId> : FileDto<TId, byte[]>
+    {
+    }
     public class FileDto : FileDto<int?>
+    {
+    }
+    public class Base64StringFileDto : FileDto<int?, string>
     {
     }
 }
