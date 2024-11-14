31c7035a03e9d6345ef90a9a1709d4b8c1ff504c
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-11-14 13:43:10 +0400

    Added Url to file dto

--- src/Zek/Contracts/FileDto.cs
+++ src/Zek/Contracts/FileDto.cs
@@ -14,6 +14,7 @@ namespace Zek.Contracts
         public string Hash { get; set; }
         public DateTime? CreateDate { get; set; }
         public bool? IsDeleted { get; set; }
+        public string Url { get; set; }
     }
 
     public class FileDto<TId> : FileDto<TId, byte[]>
