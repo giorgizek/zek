00305fdc941399b40fea72438b88197ce7d22add
Giorgi Zekalashvili <giorgizek@gmail.com>
2025-09-15 15:16:58 +0400

    Update Program.cs

--- src/Zek.Test/Program.cs
+++ src/Zek.Test/Program.cs
@@ -3,12 +3,24 @@ using System.Security.Cryptography;
 using System.Text;
 using Zek.Test;
 using Zek.Utils;
+using Zek.Web;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
+
+
+
+        var url = @"https://api.openphone.com/v1/messages?phoneNumberId=PNF4jWl6s5&participants=%2B14807122318&maxResults=10";
+
+        var header = new Dictionary<string, string>
+        {
+            { "Authorization" , "888t2WVWTceRHzOQM3olzl93mkZixNBz" }
+};
+        HttpClientUtility.PostAsJsonAsync(url, null, null, false).GetAwaiter().GetResult();
+
 
         string input = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"; // Example input
 
